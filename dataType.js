f3cd6b638f42d4efe6f9e1f9ba3ee3cee7e70ebe
Akash Shettigar <akashshettigar97@gmail.com>
2022-03-24 14:22:42 +0530

    created data type file

---
+++ dataType.js
@@ -0,0 +1,18 @@
+var num; //number data type
+num = 10;
+console.log(num);
+var strVal; //string data type
+strVal = "hello";
+console.log(strVal);
+var arrNum; //array of numbers
+arrNum = [1, 2, 3, 4];
+console.log(arrNum);
+var arrStr; //arrray of string
+arrStr = ['hello', 'world'];
+console.log(arrStr);
+var anyType; //any datatype
+anyType = 10;
+console.log(anyType);
+var anyData; //any datatype
+anyType = "hai";
+console.log(anyData);
---
+++ datatype.ts
@@ -0,0 +1,26 @@
+let num : number;       //number data type
+num=10;
+console.log(num);
+
+
+let strVal : string;    //string data type
+strVal="hello";
+console.log(strVal);
+
+let arrNum: number[];   //array of numbers
+arrNum=[1,2,3,4];
+console.log(arrNum);
+
+let arrStr: string[];       //arrray of string
+arrStr=['hello', 'world']
+console.log(arrStr);
+
+let anyType: any;           //any datatype
+anyType=10;
+console.log(anyType);
+
+let anyData: any;           //any datatype
+anyType="hai";
+console.log(anyData)
+
+
